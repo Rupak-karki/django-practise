22a1742e80f43a9dade0f041cd40ecc5537acaa9
Rupak-karki <karkirupak137@gmail.com>
2026-04-29 17:35:56 +0545

    Added medicine from admin and fetching data from it.

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Medicine
+
+admin.site.register(Medicine)
+
 
 # Register your models here.
--- core/views.py
+++ core/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 
-def home (request):
-    return render(request, 'home.html')
+from .models import Medicine
+
+def home(request):
+    medicines = Medicine.objects.all()
+    return render(request, 'home.html', {'medicines': medicines})
 def about(request):
     return render(request, 'about.html')
